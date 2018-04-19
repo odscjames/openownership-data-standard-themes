dacb5ca7dd3f26764bf976cf6392e12309f25a0c
James Baster <james.baster@opendataservices.coop>
2018-04-19 11:22:28 +0100

    We build our own CSS from bootstrap source files!

---
+++ bootstrap_build/GruntFile.js
@@ -0,0 +1,31 @@
+var request = require('request');
+
+module.exports = function(grunt) {
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compact' // Can be nested, compact, compressed, expanded.
+                },
+                files: [{
+                    expand: true, // Recursive Output style.
+                    cwd: "bootstrap/scss/", // The startup directory
+                    src: ["**/*.scss"], // Source files
+                    dest: "../oods/static/bootstrap-4.0.0/css/", // Destination
+                    ext: ".css" // File extension
+                }]
+            }
+        }
+
+    });
+    grunt.registerTask('default', ['sass']);
+}
---
+++ bootstrap_build/README.md
@@ -0,0 +1,28 @@
+# Bootstrap Build
+
+You'll need
+
+  *  npm
+  *  sass 3.5+
+
+In this directory run
+
+    git clone https://github.com/twbs/bootstrap.git  bootstrap
+    cd bootstrap && git checkout v4.0.0 && cd ..
+    
+Gulp-cli should be installed globally
+
+    sudo npm install -g grunt-cli
+
+Then install needed packages
+
+    npm install
+
+To build run
+
+    grunt sass
+
+
+## Credits
+
+With heavy help from https://github.com/pberrecloth/ods_standards_lab
